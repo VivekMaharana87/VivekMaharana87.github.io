4c09c1b36639b80fb4c4bddf53b7397579771925
Vivek Vardhan Maharana <79391371+VivekMaharana87@users.noreply.github.com>
2026-03-10 21:20:42 +0530

    Add files via upload

---
+++ .next/dev/static/chunks/app_page_tsx_24540477._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_19b301ba._.js",
+  "static/chunks/_31a3df4f._.js"
+],
+    source: "dynamic"
+});
